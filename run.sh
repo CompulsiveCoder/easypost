d40951201db93f3c722ce06b0b0a231b9d618e30
CompulsiveCoder <compulsivecoder@gmail.com>
2016-06-22 20:02:45 +1000

    Added scripts

---
+++ run.sh
@@ -0,0 +1,5 @@
+echo "Running easypost project"
+echo "Dir: $PWD"
+
+cd src/easypost.WWW
+xsp4 --port 8082
---
+++ test-from-github.sh
@@ -0,0 +1,16 @@
+BRANCH=$1
+
+if [ -z "$BRANCH" ]; then
+    BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+fi
+
+if [ -z "$BRANCH" ]; then
+    BRANCH="master"
+fi
+
+echo "Branch: $BRANCH"
+
+git clone https://github.com/CompulsiveCoder/easypost.git --branch $BRANCH
+cd easypost
+
+sh init-build-test.sh
